2a6c9d24df50b8b8e867ecca8a103f30770659dd
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-12 16:39:42 -0300

    agrega campos de formulario

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,8 +17,38 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
           onClick={cerrarModal}
         />
       </div>
-      <form className={`formulario ${animarModal ? 'animar' : ''}`}>
+      <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
         <legend>Nuevo Gasto</legend>
+        <div className='campo'>
+          <label htmlFor='nombre'>Nombre Gasto</label>
+          <input
+            id='nombre'
+            type='text'
+            placeholder='Nombre del gasto'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='cantidad'>Cantidad</label>
+          <input
+            id='cantidad'
+            type='text'
+            placeholder='Ingresa la cantidad. Ej: 1000'
+          />
+        </div>
+        <div className='campo'>
+          <label htmlFor='cantidad'>Categoría</label>
+          <select id='categoria'>
+            <option value=''>--Seleccione--</option>
+            <option value='vivienda'>Vivienda</option>
+            <option value='salud'>Salud</option>
+            <option value='entretencion'>Entretención</option>
+            <option value='otros'>Otros</option>
+          </select>
+        </div>
+        <input
+          type='submit'
+          value='Agregar Gasto'
+        />
       </form>
     </div>
   )
