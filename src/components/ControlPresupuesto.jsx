c061c1c3d867b7aa98807d709eb233eeccd70e15
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-03-13 16:10:23 -0300

    calculo disponible y gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,8 +1,22 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import MyContext from '../my_context'
 
 const ControlPresupuesto = () => {
-  const { presupuesto } = useContext(MyContext)
+  const { presupuesto, gastos } = useContext(MyContext)
+  const [disponible, setDisponible] = useState(0)
+  const [gastado, setGastado] = useState(0)
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => total + gasto.cantidad,
+      0
+    )
+    setGastado(totalGastado)
+
+    const totalDisponible = presupuesto - totalGastado
+    setDisponible(totalDisponible)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gastos])
 
   const formatearCantidad = cantidad => {
     return cantidad.toLocaleString('es-CL', {
@@ -19,10 +33,10 @@ const ControlPresupuesto = () => {
           <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible: </span> {formatearCantidad(0)}
+          <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado: </span> {formatearCantidad(0)}
+          <span>Gastado: </span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
